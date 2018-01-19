b903819160f5f1fd7d840379e90c77e50a9e7ce0
Abhishek Kaushal <abhikaushal07@gmail.com>
2018-01-19 17:45:16 +0530

    outright forward done
    new changes done

--- fxmmSmokeTests/FXMMTestScriptNEW.java
+++ fxmmSmokeTests/FXMMTestScriptNEW.java
@@ -9,10 +9,13 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.server.handler.FindElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -22,7 +25,7 @@ import org.testng.annotations.BeforeClass;
 public class FXMMTestScriptNEW {
 	
 	WebDriver driver;
-	String URL = "https://bgamodel.na.dir.bunge.com/fxmm";   
+	String URL = "https://bga-ux-wsd1.na.dir.bunge.com/fxmm/";   
     
 	
   @BeforeClass
@@ -33,6 +36,7 @@ FirefoxOptions dc = new FirefoxOptions();     //DesiredCapabilities dc = Desired
 dc.setCapability("marionette", true);
 dc.setCapability("acceptInsecureCerts", true); 
 driver = new FirefoxDriver(dc);
+//driver.manage().deleteAllCookies();
 driver.get(URL);
 driver.manage().timeouts().implicitlyWait(30000, TimeUnit.MILLISECONDS);
 
@@ -46,14 +50,14 @@ driver.findElement(By.name("USER")).clear();
 driver.findElement(By.name("USER")).sendKeys("UMASING");
 driver.findElement(By.name("PASSWORD")).clear();
 driver.findElement(By.name("PASSWORD")).sendKeys("Bunge123");
-findElement("html/body/table[1]/tbody/tr[3]/td/form/p/table/tbody/tr[1]/td/table/tbody/tr[4]/td/input");
+find_Click("html/body/table[1]/tbody/tr[3]/td/form/p/table/tbody/tr[1]/td/table/tbody/tr[4]/td/input");
 Thread.sleep(1000);
 Assert.assertEquals("FXMM - FX & Money Markets", driver.getTitle()	, "Page Title NOT VERIFIED");
-findElement(".//*[@id='buttons']/div/ul/li[1]/a");
-findElement(".//*[@id='buttons']/div/ul/li[1]/ul/li[1]/a");
-findElement(".//*[@id='form']/div[1]/div[1]/div/div/button");
-findElement(".//*[@id='form']/div[1]/div[1]/div/div/div/ul/li[3]/a");
-findElement(".//*[@id='searchButton']");
+find_Click(".//*[@id='buttons']/div/ul/li[1]/a");
+find_Click(".//*[@id='buttons']/div/ul/li[1]/ul/li[1]/a");
+find_Click(".//*[@id='form']/div[1]/div[1]/div/div/button");
+find_Click(".//*[@id='form']/div[1]/div[1]/div/div/div/ul/li[3]/a");
+find_Click(".//*[@id='searchButton']");
 
 Thread.sleep(7000);
 String Results = driver.findElement(By.xpath(".//*[@id='pager']/div/span[3]")).getText();
@@ -80,39 +84,77 @@ else
 @Test (priority =2) 
 public void TestOutrightForward() throws InterruptedException {
 	
-	driver.get("https://bgamodel.na.dir.bunge.com/fxmm/dealCapture/fxSwap/main");
+	driver.get("https://bga-ux-wsd1.na.dir.bunge.com/fxmm/dealCapture/outrightForward/main");
 	Thread.sleep(3000);
-	findElement(".//*[@id='form']/fieldset[2]/div[1]/div/img");
+	find_Click(".//*[@id='form']/fieldset[2]/div[1]/div/img");
 	Thread.sleep(2000);
-	findElement(".//*[@id='folderListDialog']/div/div[2]/table/tbody/tr[1]/td[1]");
+	find_Click(".//*[@id='folderListDialog']/div/div[2]/table/tbody/tr[1]/td[1]");
 	Thread.sleep(2000);
-	findElement(".//*[@id='form']/fieldset[2]/div[2]/div/img");
+	find_Click(".//*[@id='form']/fieldset[2]/div[2]/div/img");
 	Thread.sleep(2000);
-	findElement(".//*[@id='counterpartyListDialog']/div/div[2]/table/tbody/tr[1]/td[1]");
+	find_Click(".//*[@id='counterpartyListDialog']/div/div[2]/table/tbody/tr[1]/td[1]");
+	Thread.sleep(2000);
+	find_Click("//img[@onclick='dealCommons.showProfileListDialog()']");
+	Thread.sleep(1000);
+	find_Click(".//*[@id='profileListDialog']/div/div[2]/table/tbody/tr[1]/td");
+	Thread.sleep(2000);
+	find_Send(".//*[@id='maturityDate']", "11/04/2018");
+	find_Send(".//*[@id='spotRate']", "11");
+	find_Send(".//*[@id='forwardRate']", "11");
+	find_Send(".//*[@id='forwardPoints']", "1");
+	find_Send(".//*[@id='amount1']", "100");
+	find_Click(".//*[@id='insertButton']");
+	Thread.sleep(2000);
+	find_Click(".//*[@id='confirmDialogYesButton']");
+	WebElement Loading = driver.findElement(By.xpath(".//*[@id='loadingModalTitle']"));
+	
+	if (Loading.isDisplayed()) {
+		Thread.sleep(10000);
+		assertEquals(driver.findElement(By.xpath(".//*[@id='successMessageDiv']/b")).getText(), "Outright Forward Deal inserted successfully");
+		System.out.println("Element INSERTED SUCCESSFULLY");
+	}
+	
+	else {
+	assertEquals(driver.findElement(By.xpath(".//*[@id='successMessageDiv']/b")).getText(), "Outright Forward Deal inserted successfully");
+	System.out.println("Element INSERTED SUCCESSFULLY");
+	}
+	Thread.sleep(1000);
+	String DealID = driver.findElement(By.xpath(".//*[@id='dealId']")).getAttribute("value");
+	System.out.println(DealID);
+	
 }
     
   
   
   @AfterClass
   public void afterMethod() {
-  driver.close();
+ 
+	  driver.quit();
+//	  driver.close();
 
   }
 
   
   
   
-  public void SelectEment(String xpath, String selectBytext) {
+  public void SelectElement(String xpath, String selectBytext) {
 
 		Select ele1= new Select(driver.findElement(By.xpath(xpath)));
 			ele1.selectByVisibleText(selectBytext);
 		}
   
-  public void findElement(String Xpath) {
+  public void find_Click(String Xpath) {
 	  
-	driver.findElement(By.xpath(Xpath)).click();;  
+	driver.findElement(By.xpath(Xpath)).click();  
   }
   
+
+  public void find_Send(String Xpath, String y) {
+	  
+	driver.findElement(By.xpath(Xpath)).sendKeys(y); 
+  }
+
+
+
 }
 
-;
